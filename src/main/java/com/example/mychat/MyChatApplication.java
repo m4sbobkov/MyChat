a2ab49ad710d162eb9eccd497d66c63a0b6c810a
m4sbobkov <alexbob14@mail.ru>
2021-11-22 13:22:25 +0400

    sdfg

--- src/main/java/com/example/mychat/MyChatApplication.java
+++ src/main/java/com/example/mychat/MyChatApplication.java
@@ -7,15 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class MyChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyChatApplication.class.getResource("MyChat-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("ChatVolchat");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/mychat/MyChatController.java
+++ src/main/java/com/example/mychat/MyChatController.java
@@ -1,14 +1,26 @@
 package com.example.mychat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MyChatController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextArea mainTextArea;
+
+
+    @FXML
+    TextField message;
+
+
+    @FXML
+    protected void onSendButtonClick(ActionEvent actionEvent) {
+        if (!message.getText().isEmpty()) {
+            mainTextArea.appendText(message.getText() + "\n");
+            message.clear();
+        }
     }
 }
