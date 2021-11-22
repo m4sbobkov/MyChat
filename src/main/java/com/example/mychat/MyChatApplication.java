60405460c99e9231b479481a6306ff65f79573bc
m4sbobkov <alexbob14@mail.ru>
2021-11-22 13:20:01 +0400

    Initial commit

---
+++ src/main/java/com/example/mychat/MyChatApplication.java
@@ -0,0 +1,23 @@
+package com.example.mychat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyChatApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MyChatApplication.class.getResource("MyChat-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.mychat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.mychat to javafx.fxml;
+    exports com.example.mychat;
+}
